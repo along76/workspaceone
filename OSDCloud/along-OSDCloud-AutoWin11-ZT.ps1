b3686974f7f59fa049e9d3bd3f573d30e3fc55f4
along76 <45182606+along76@users.noreply.github.com>
2022-04-09 19:23:55 -0700

    Update along-OSDCloud-AutoWin11-ZT.ps1

--- OSDCloud/along-OSDCloud-AutoWin11-ZT.ps1
+++ OSDCloud/along-OSDCloud-AutoWin11-ZT.ps1
@@ -1,1 +1,18 @@
+Write-Host  -ForegroundColor Yellow "Starting along' Custom OSDCloud ..."
+cls
+Write-Host "===================== Main Menu =======================" -ForegroundColor Yellow
+Write-Host "==================    Sup Foo      ====================" -ForegroundColor Yellow
+Write-Host "====================  @along76  =======================" -ForegroundColor Yellow
+Write-Host "=======================================================" -ForegroundColor Yellow
 
+Write-Host "Zero-Touch Win11 21H2 | English | Education" -ForegroundColor Yellow
+
+Write-Host  -ForegroundColor Yellow "Loading OSDCloud..."
+
+Import-Module OSD -Force
+Install-Module OSD -Force
+
+Start-OSDCloud -OSVersion 'Windows 11' -OSLanguage en-us -OSBuild 21H2 -OSEdition Education -ZTI 
+ 
+
+wpeutil reboot
